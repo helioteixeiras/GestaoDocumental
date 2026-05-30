4d486ee9ca701b49a32f414c6ebde7e18bb277b2
Htadmin <helioteixeiras@gmail.com>
2026-05-30 23:00:50 +0100

    Criar dashboard inicial e configurar rotas

--- frontend/gestao-documental-web/src/app/app.routes.ts
+++ frontend/gestao-documental-web/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './features/dashboard/pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard
+  }
+];
